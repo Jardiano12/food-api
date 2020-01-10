5dfdc988841c60e6c9158d8056afa6a5bf13323a
Jardiano12 <jardiano.ifrr.tcc@gmail.com>
2020-01-10 12:29:35 -0300

    Resolvendo bug da classe CadastroCozinhaService

--- src/main/java/com/jardiano/food/api/api/controller/CozinhaController.java
+++ src/main/java/com/jardiano/food/api/api/controller/CozinhaController.java
@@ -1,6 +1,7 @@
 package com.jardiano.food.api.api.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,17 +35,17 @@ public class CozinhaController {
 	
 	@GetMapping
 	public List<Cozinha> listar() {
-		return cozinhaRepository.listar();
+		return cozinhaRepository.findAll();
 	}
-	
+
 	@GetMapping("/{cozinhaId}")
 	public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
-		Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
-		
-		if (cozinha != null) {
-			return ResponseEntity.ok(cozinha);
+		Optional<Cozinha> cozinha = cozinhaRepository.findById(cozinhaId);
+
+		if (cozinha.isPresent()) {
+			return ResponseEntity.ok(cozinha.get());
 		}
-		
+
 		return ResponseEntity.notFound().build();
 	}
 	
@@ -53,31 +54,31 @@ public class CozinhaController {
 	public Cozinha adicionar(@RequestBody Cozinha cozinha) {
 		return cadastroCozinha.salvar(cozinha);
 	}
-	
+
 	@PutMapping("/{cozinhaId}")
 	public ResponseEntity<Cozinha> atualizar(@PathVariable Long cozinhaId,
-			@RequestBody Cozinha cozinha) {
-		Cozinha cozinhaAtual = cozinhaRepository.buscar(cozinhaId);
-		
-		if (cozinhaAtual != null) {
-			BeanUtils.copyProperties(cozinha, cozinhaAtual, "id");
-			
-			cozinhaAtual = cadastroCozinha.salvar(cozinhaAtual);
-			return ResponseEntity.ok(cozinhaAtual);
+											 @RequestBody Cozinha cozinha) {
+		Optional<Cozinha> cozinhaAtual = cozinhaRepository.findById(cozinhaId);
+
+		if (cozinhaAtual.isPresent()) {
+			BeanUtils.copyProperties(cozinha, cozinhaAtual.get(), "id");
+
+			Cozinha cozinhaSalva = cadastroCozinha.salvar(cozinhaAtual.get());
+			return ResponseEntity.ok(cozinhaSalva);
 		}
-		
+
 		return ResponseEntity.notFound().build();
 	}
-	
+
 	@DeleteMapping("/{cozinhaId}")
 	public ResponseEntity<?> remover(@PathVariable Long cozinhaId) {
 		try {
-			cadastroCozinha.excluir(cozinhaId);	
+			cadastroCozinha.excluir(cozinhaId);
 			return ResponseEntity.noContent().build();
-			
+
 		} catch (EntidadeNaoEncontradaException e) {
 			return ResponseEntity.notFound().build();
-			
+
 		} catch (EntidadeEmUsoException e) {
 			return ResponseEntity.status(HttpStatus.CONFLICT)
 					.body(e.getMessage());
--- src/main/java/com/jardiano/food/api/domain/repository/CozinhaRepository.java
+++ src/main/java/com/jardiano/food/api/domain/repository/CozinhaRepository.java
@@ -2,18 +2,20 @@ package com.jardiano.food.api.domain.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 import com.jardiano.food.api.domain.model.Cozinha;
+import org.springframework.stereotype.Repository;
 
-@Component
-public interface CozinhaRepository {
+@Repository
+public interface CozinhaRepository extends JpaRepository<Cozinha, Long> {
 
-	List<Cozinha> listar();
+/*	List<Cozinha> listar();
 	List<Cozinha> consultarPorNome(String nome);
 	Cozinha buscar(Long id);
 
 
 	Cozinha salvar(Cozinha cozinha);
-	void remover(Long id);
+	void remover(Long id);*/
 
 }
